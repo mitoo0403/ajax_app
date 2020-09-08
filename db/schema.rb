023657a2f19707cbe6e5bbe89905f5b9391fdc75
mitoo0403 <mitooo.19821113@gmail.com>
2020-09-08 14:26:05 +0900

    既読機能のフロントエンド実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_074947) do
+ActiveRecord::Schema.define(version: 2020_09_08_024055) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
